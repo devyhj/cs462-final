541099b8dd8a741eec767a06af000d949c38869b
Hojune Yoo <soy0803@gmail.com>
2016-04-12 00:57:44 -0600

    event fix

--- app/Console/Commands/CheckIncidents.php
+++ app/Console/Commands/CheckIncidents.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Event;
 use App\Events\IncidentFound;
+use App\Incident;
 
 class CheckIncidents extends Command
 {
@@ -47,7 +48,48 @@ class CheckIncidents extends Command
         $result = json_decode(curl_exec($ch));
         curl_close($ch); 
         $incidents = $result->resourceSets[0]->resources;
-        Event::fire(new IncidentFound($incidents));
+
+        $newIncidents = collect();
+
+        foreach($incidents as $incident)
+        {
+            // dd($incident);
+            $points = $incident->point->coordinates;
+            $googleKey = 'AIzaSyBY8R1d3q5xNLyJo63MGr_SINqHD7Ulgco';
+            $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.$points[0].','.$points[1]."&key=".$googleKey;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url); 
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+            //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+            $result = json_decode(curl_exec($ch));
+            curl_close($ch);
+            // dd($result);
+            $external_id = $incident->incidentId;
+            $description = $incident->description;
+            $address = $result->results[0]->formatted_address;
+            $count = Incident::where('external_id', $external_id)->count();
+            
+            /* Test Block
+            $external_id = 123456;
+            $description = 'WINDOWS IS TERRIBLE';
+            $address = '224 Meh Way, Somewhere Nowhere';
+            $count = Incident::where('external_id', $external_id)->count();
+            */
+            if($count == 0) //if new incident
+            {
+                $newIncident = new Incident;
+                $newIncident->location = $address;
+                $newIncident->description = $description;
+                $newIncident->external_id=$external_id;
+                $newIncident->save();
+
+                $newIncidents->push($newIncident);
+            }
+        }
+
+
+        Event::fire(new IncidentFound($newIncidents));
     }
     
 }
--- app/Listeners/AlertClient.php
+++ app/Listeners/AlertClient.php
@@ -8,6 +8,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Twilio;
 use App\Incident;
+use App\User;
 
 class AlertClient
 {
@@ -30,48 +31,17 @@ class AlertClient
     public function handle(IncidentFound $event)
     {
         $incidents = $event->getIncidents();
+        $users = User::all();
         foreach($incidents as $incident)
         {
-            // dd($incident);
-            $points = $incident->point->coordinates;
-            $googleKey = 'AIzaSyBY8R1d3q5xNLyJo63MGr_SINqHD7Ulgco';
-            $url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='.$points[0].','.$points[1]."&key=".$googleKey;
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url); 
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-            //curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-            //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-            $result = json_decode(curl_exec($ch));
-            curl_close($ch);
-            // dd($result);
-            $external_id = $incident->incidentId;
-            $description = $incident->description;
-            $address = $result->results[0]->formatted_address;
-            $count = Incident::where('external_id', $external_id)->count();
-            
-            /* Test Block
-            $external_id = 123456;
-            $description = 'WINDOWS IS TERRIBLE';
-            $address = '224 Meh Way, Somewhere Nowhere';
-            $count = Incident::where('external_id', $external_id)->count();
-            */
-            if($count == 0) //if new incident
-            {
-                $newIncident = new Incident;
-                $newIncident->location = $address;
-                $newIncident->description = $description;
-                $newIncident->external_id=$external_id;
-                $newIncident->save();
-                $users = DB::select('select * from users');
+            $message = '';
 
-                $message = '';
-
-                $message = 'Location: '.$address.' / Description: '.$description;
-                foreach($users as $user){
-                    // If you're getting SSL cert errors, add `CURLOPT_SSL_VERIFYPEER => FALSE,` after line 59 in --> vendor/twilio/sdk/services/twilio/TinyHttp.php
-                    Twilio::message($user->phone_number, $message);
-                }
+            $message = 'Location: '.$incident->location.' / Description: '.$incident->description;
+            foreach($users as $user){
+                // If you're getting SSL cert errors, add `CURLOPT_SSL_VERIFYPEER => FALSE,` after line 59 in --> vendor/twilio/sdk/services/twilio/TinyHttp.php
+                Twilio::message($user->phone_number, $message);
             }
+            
         }
         // Twilio::message($event->user->phone, $event->message);
     }
